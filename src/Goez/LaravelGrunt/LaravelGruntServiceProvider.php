7cc316657a3a986c7e67bfdd32b109b5344251b6
Jace Ju <jaceju@gmail.com>
2013-11-11 17:45:44 +0800

    Complete service provider.

--- src/Goez/LaravelGrunt/LaravelGruntServiceProvider.php
+++ src/Goez/LaravelGrunt/LaravelGruntServiceProvider.php
@@ -3,6 +3,17 @@
 namespace Goez\LaravelGrunt;
 
 use Illuminate\Support\ServiceProvider;
+use Goez\LaravelGrunt\Grunt\Gruntfile;
+use Goez\LaravelGrunt\Grunt\GruntGenerator;
+use Goez\LaravelGrunt\Bower\Bowerfile;
+use Goez\LaravelGrunt\Bower\BowerGenerator;
+use Goez\LaravelGrunt\Commands\GruntSetupCommand;
+use Goez\LaravelGrunt\Commands\GruntBuildCommand;
+use Goez\LaravelGrunt\Commands\GruntWatchCommand;
+use Goez\LaravelGrunt\Commands\GruntConfigCommand;
+use Goez\LaravelGrunt\Commands\BowerSetupCommand;
+use Goez\LaravelGrunt\Commands\BowerInstallCommand;
+use Goez\LaravelGrunt\Commands\BowerUpdateCommand;
 
 class LaravelGruntServiceProvider extends ServiceProvider
 {
@@ -20,7 +31,132 @@ class LaravelGruntServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Register Grunt Related Stuff
+        $this->registerGruntConfigCommand();
+        $this->registerGruntSetupCommand();
+        $this->registerGruntBuildCommand();
+        $this->registerGruntWatchCommand();
+
+        // Register Bower Related Stuff
+        $this->registerBowerSetupCommand();
+        $this->registerBowerInstallCommand();
+        $this->registerBowerUpdateCommand();
+
+        // Register Artisan Commands
+        $this->registerCommands();
+    }
+
+    /**
+     * Register the grunt.setup command to the IoC
+     *
+     * @return \Goez\LaravelGrunt\Commands\GruntSetupCommand
+     */
+    public function registerGruntSetupCommand()
+    {
+        $this->app['grunt.setup'] = $this->app->share(function($app) {
+            $gruntFile = new Gruntfile($app['files'], $app['config']);
+            $gruntGenerator = new GruntGenerator($app['files'], $gruntFile, $app['config']);
+
+            return new GruntSetupCommand($gruntGenerator, $app['config']);
+        });
+    }
+
+    /**
+     * Register the grunt.build command to the IoC
+     *
+     * @return \Goez\LaravelGrunt\Commands\GruntBuildCommand
+     */
+    public function registerGruntBuildCommand()
+    {
+        $this->app['grunt.build'] = $this->app->share(function($app) {
+            return new GruntBuildCommand();
+        });
+    }
+
+    /**
+     * Register the grunt.config command to the IoC
+     *
+     * @return \Goez\LaravelGrunt\Commands\GruntConfigCommand
+     */
+    public function registerGruntConfigCommand()
+    {
+        $this->app['grunt.config'] = $this->app->share(function($app) {
+            return new GruntConfigCommand();
+        });
+    }
+
+    /**
+     * Register the grunt.watch command to the IoC
+     *
+     * @return \Goez\LaravelGrunt\Commands\GruntWatchCommand
+     */
+    public function registerGruntWatchCommand()
+    {
+        $this->app['grunt.watch'] = $this->app->share(function($app) {
+            return new GruntWatchCommand();
+        });
+    }
+
+    /**
+     * Register the bower.setup command to the IoC
+     *
+     * @return \Goez\LaravelGrunt\Commands\BowerSetupCommand
+     */
+    public function registerBowerSetupCommand()
+    {
+        $this->app['bower.setup'] = $this->app->share(function($app) {
+            $bowerFile = new Bowerfile($app['files'], $app['config']);
+            $bowerGenerator = new BowerGenerator($app['files'], $bowerFile, $app['config']);
+
+            return new BowerSetupCommand($bowerGenerator, $app['config']);
+        });
+    }
+
+    /**
+     * Register the bower.install command to the IoC
+     *
+     * @return \Goez\LaravelGrunt\Commands\BowerInstallCommand
+     */
+    public function registerBowerInstallCommand()
+    {
+        $this->app['bower.install'] = $this->app->share(function($app) {
+            return new BowerInstallCommand();
+        });
+    }
+
+    /**
+     * Register the bower.update command to the IoC
+     *
+     * @return \Goez\LaravelGrunt\Commands\BowerUpdateCommand
+     */
+    public function registerBowerUpdateCommand()
+    {
+        $this->app['bower.update'] = $this->app->share(function($app) {
+            return new BowerUpdateCommand();
+        });
+    }
+
+    public function registerCommands()
+    {
+        $this->commands(
+            'grunt.setup',
+            'grunt.build',
+            'grunt.config',
+            'grunt.watch',
+            'bower.setup',
+            'bower.install',
+            'bower.update'
+        );
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('jason-nz/laravel-grunt');
     }
 
     /**
