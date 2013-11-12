6f8cf8eddacabfd6a9589c7515cb5a3465a9e888
Jace Ju <jaceju@gmail.com>
2013-11-12 11:26:19 +0800

    Remove 'bower:setup' command.

--- src/Goez/LaravelGrunt/LaravelGruntServiceProvider.php
+++ src/Goez/LaravelGrunt/LaravelGruntServiceProvider.php
@@ -9,7 +9,6 @@ use Goez\LaravelGrunt\Bower\Bowerfile;
 use Goez\LaravelGrunt\Bower\BowerGenerator;
 use Goez\LaravelGrunt\Commands\GruntConfigCommand;
 use Goez\LaravelGrunt\Commands\GruntSetupCommand;
-use Goez\LaravelGrunt\Commands\BowerSetupCommand;
 
 class LaravelGruntServiceProvider extends ServiceProvider
 {
@@ -29,7 +28,6 @@ class LaravelGruntServiceProvider extends ServiceProvider
     {
         $this->registerGruntConfigCommand();
         $this->registerGruntSetupCommand();
-        $this->registerBowerSetupCommand();
         $this->registerCommands();
     }
 
@@ -60,27 +58,11 @@ class LaravelGruntServiceProvider extends ServiceProvider
         });
     }
 
-    /**
-     * Register the bower.setup command to the IoC
-     *
-     * @return \Goez\LaravelGrunt\Commands\BowerSetupCommand
-     */
-    public function registerBowerSetupCommand()
-    {
-        $this->app['bower.setup'] = $this->app->share(function($app) {
-            $bowerFile = new Bowerfile($app['files'], $app['config']);
-            $bowerGenerator = new BowerGenerator($app['files'], $bowerFile, $app['config']);
-
-            return new BowerSetupCommand($bowerGenerator, $app['config']);
-        });
-    }
-
     public function registerCommands()
     {
         $this->commands(
             'grunt.config',
-            'grunt.setup',
-            'bower.setup'
+            'grunt.setup'
         );
     }
 
