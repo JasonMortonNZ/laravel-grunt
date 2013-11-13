a76eb3841f8821fffef6f512ebe0b27b01e2379b
Jace Ju <jaceju@gmail.com>
2013-11-14 00:44:50 +0800

    Add RequireJS module.

---
+++ src/Goez/LaravelGrunt/Metafile/Requirejs.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Goez\LaravelGrunt\Metafile;
+
+use Goez\LaravelGrunt\Metafile;
+
+class Requirejs extends Metafile
+{
+
+    /**
+     * @var string
+     */
+    protected $assetsPath = '';
+
+    /**
+     * @return void
+     */
+    public function init()
+    {
+        $this->assetsPath = $this->config->get('laravel-grunt::assets_path');
+    }
+
+    /**
+     * @return array
+     */
+    public function fileNames()
+    {
+        return array();
+    }
+
+    /**
+     * @return array
+     */
+    public function manifest()
+    {
+        return array(
+            $this->assetsPath . '/scripts' => static::DIR,
+            $this->assetsPath . '/scripts/app.js' => static::TPL . ':requirejs/app.js.txt',
+            $this->assetsPath . '/scripts/main.js' => static::TPL . ':requirejs/main.js.txt',
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function postCommands()
+    {
+        return array();
+    }
+}
