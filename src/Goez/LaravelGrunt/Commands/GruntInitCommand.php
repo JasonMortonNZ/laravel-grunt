98fa00b1cbcb222ded57736a684499a4a146f3a8
Jace Ju <jaceju@gmail.com>
2013-11-14 00:53:36 +0800

    Remove unused code.

--- src/Goez/LaravelGrunt/Commands/GruntInitCommand.php
+++ src/Goez/LaravelGrunt/Commands/GruntInitCommand.php
@@ -125,30 +125,6 @@ class GruntInitCommand extends Command
             }
         }
 
-//        // Check if a gruntfile.js or package.json already exists
-//        foreach ($this->generators as $generator)
-//        if ($generator->filesExist()) {
-//            if (! $this->askContinue($generator)) {
-//                return;
-//            }
-//        }
-//
-//        // Check if node and npm are installed
-//        $this->info('Node and NPM are installed. Continue...');
-//        $this->askQuestions();
-//
-//        // Create package.json file & generate custom gruntfile.js
-//        foreach ($this->generators as $generator) {
-//            $generator->generate($this->plugins);
-//        }
-//        $this->info('Metadata files successfully created!');
-//
-//        $this->info('Install bower dependences...');
-//        $this->installBowerDependences();
-//
-//        $this->info('Installing / updating required grunt plugins...');
-//        $this->installGruntPlugins();
-
     }
 
     protected function checkRequirement($command, $check)
@@ -193,7 +169,6 @@ class GruntInitCommand extends Command
         }
     }
 
-
     /**
      * Add the custom options to content
      *
@@ -210,98 +185,5 @@ class GruntInitCommand extends Command
 
         return $content;
     }
-
-//    /**
-//     * Files already exist, do you want to continue?
-//     *
-//     * @param  GeneratorInterface $generator
-//     * @return boolean
-//     */
-//    protected function askContinue(GeneratorInterface $generator)
-//    {
-//        $filenames = $generator->getFilenames();
-//
-//        return ($this->confirm(
-//            'A ' . implode(' or ', $filenames) .
-//            ' file already exist and will be replaced.' .
-//            ' Do you want to continue? [y|n]', false));
-//    }
-//
-//    /**
-//     * Ask the user which plugins they require
-//     *
-//     * @return void
-//     */
-//    protected function askQuestions()
-//    {
-//        // Do they want preprocessing?
-//        $this->wantPreprocessing();
-//    }
-//
-//    /**
-//     * Ask user which preprocessor they require
-//     *
-//     * @return void
-//     */
-//    protected function wantPreprocessing()
-//    {
-//        if ($this->confirm('Do you require CSS preprocessing? [y|n]', false)) {
-//            // Get answer from user
-//            $preprocessor = strtolower($this->ask('Which CSS preprocessor do you require? [less|sass|stylus]'));
-//
-//            // While answer is not valid, ask again
-//            while ( ! $preprocessor || ! in_array($preprocessor, array('less', 'sass', 'stylus'))) {
-//                $preprocessor = $this->ask('I did not recognize that preprocessor. Please try again. [less|sass|stylus]');
-//            }
-//
-//            $this->plugins[] = $preprocessor;
-//        }
-//    }
-//
-//    /**
-//     * Check if user has node installed
-//     *
-//     * @return boolean
-//     */
-//    protected function hasNode()
-//    {
-//        $node = shell_exec('node -v');
-//
-//        return starts_with($node, 'v');
-//    }
-//
-//    /**
-//     * Check if user has npm (node package manager) installed
-//     *
-//     * @return boolean
-//     */
-//    protected function hasNpm()
-//    {
-//        $npm = shell_exec('npm -v');
-//
-//        return starts_with($npm, '1.');
-//    }
-//
-//    /**
-//     * Check if user has bower installed
-//     *
-//     * @return boolean
-//     */
-//    protected function hasBower()
-//    {
-//        $bower = shell_exec('bower -v');
-//
-//        return starts_with($bower, '1');
-//    }
-//
-//    protected function installBowerDependencies()
-//    {
-//        shell_exec('bower install');
-//    }
-//
-//    protected function installGruntPlugins()
-//    {
-//        shell_exec('npm install');
-//    }
 
 }
